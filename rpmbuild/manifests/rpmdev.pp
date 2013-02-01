a71104dbde344ee40f9a54458708b7d0a6f46ca5
omnifroodle <matt.overstreet@gmail.com>
2013-02-01 15:35:10 -0500

    Fixed formatting

--- rpmbuild/manifests/rpmdev.pp
+++ rpmbuild/manifests/rpmdev.pp
@@ -12,11 +12,12 @@ user { 'makerpm':
 group { 'mock':
 	ensure => 'present',
 }
+
 # Puppet doesn't yet support yum groups with the package directive
 # Until it does we use exec to install these packages. 
 exec { 'install dev tools':
-  unless  => '/usr/bin/yum grouplist "Development tools" | /bin/grep "^Installed Groups"',
-  command => '/usr/bin/yum -y groupinstall "Development tools"',
+	unless  => '/usr/bin/yum grouplist "Development tools" | /bin/grep "^Installed Groups"',
+	command => '/usr/bin/yum -y groupinstall "Development tools"',
 }
 
 package { 'rpm-build':

70e2733f1e2af1a611cf0c15785efa0a37ad3852
root <root@domU-12-31-38-04-29-DC.compute-1.internal>
2013-02-01 15:10:59 -0500

    initial commit

---
+++ manifests/rpmdev.pp
@@ -0,0 +1,25 @@
+user { 'makerpm':
+	home => '/home/makerpm',
+	shell => '/bin/bash',
+	ensure => 'present',
+	password => '*',
+	comment => 'RPM packager',
+	managehome => true,
+	groups => 'mock',
+	require => Group['mock']
+}
+
+group { 'mock':
+	ensure => 'present',
+}
+# Puppet doesn't yet support yum groups with the package directive
+# Until it does we use exec to install these packages. 
+exec { 'install dev tools':
+  unless  => '/usr/bin/yum grouplist "Development tools" | /bin/grep "^Installed Groups"',
+  command => '/usr/bin/yum -y groupinstall "Development tools"',
+}
+
+package { 'rpm-build':
+	ensure => 'present',
+	require => Exec['install dev tools']
+}
